7dfcf655cd27cd8243e16746f739289cce7887f3
Akvile Griciute <akvile.griciute@codeacademylt.onmicrosoft.com>
2022-10-02 20:31:30 +0300

    lesson 1

---
+++ lesson 1/src/event-listener.ts
@@ -0,0 +1,14 @@
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+const inputField: HTMLInputElement | null = document.querySelector('#input');
+const result: HTMLParagraphElement | null = document.querySelector('#result');
+
+if (inputField === null) throw new Error('Neteisingas įvesties lauko selektorius');
+if (result === null) throw new Error('Neteisingas rezultatų konteinerio selektorius');
+
+const handleFieldChange: EventListener = (event) => {
+  const element: HTMLInputElement = event.target as HTMLInputElement;
+  result.innerHTML = element.value;
+};
+
+inputField.addEventListener('keyup', handleFieldChange);
