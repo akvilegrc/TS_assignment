04e686116a4f91244b75bf4fb83b623184006a69
Akvile Griciute <akvile.griciute@codeacademylt.onmicrosoft.com>
2022-10-05 20:25:38 +0300

    lesson_4

--- lesson_4/uzduotys/src/literal-types.ts
+++ lesson_4/uzduotys/src/literal-types.ts
@@ -47,24 +47,58 @@ const req2 = {
 console.group('Literal types - užduotys');
 {
   // ↓↓↓↓ Tipus ir užduotims bendrus kintamuosius apraškite čia ↓↓↓↓
+  type Berlin = "Berlin";
+  type Hamburg = "Hamburg";
+  type Cologne = "Cologne";
+  type Frankfurt = "Frankfurt";
+  type Stuttgart = "Stuttgart";
 
+  type Beagle = "beagle";
+  type Poodle = "poodle";
+  type Bulldog = "bulldog";
+  type GoldenRetriever = "golden retriever";
+  type Dachshund = "dachshund";
+
+  type Bumper = "bumper";
+  type Radiator = "radiator";
+  type Dashboard = "dashboard";
+  type Spoiler = "spoiler";
+  type Tire = "tire";
   // ↑↑↑↑ Tipus ir užduotims bendrus kintamuosius apraškite čia ↑↑↑↑
 
   console.group('1. Sukurkite konkrečius 5 dižiausių Vokietijos miestų tipus. Sukurkite tipą, kurio reikšmė būtų viena iš miestų.');
   {
-    // sprendimą|sprendimo pavyzdžius spausdinkite čia 
+    type GermanCity = Berlin | Hamburg | Cologne | Frankfurt | Stuttgart;
+
+    const city1: GermanCity = "Berlin";
+    const city2: GermanCity = "Cologne";
+    const city3: GermanCity = "Stuttgart";
+
+    console.log(`${city1}, ${city2}, ${city3}`);
   }
   console.groupEnd();
 
   console.group('2. Sukurkite konkrečias 5 šunų veisles. Sukurkite tipą, kurio reikšmė būtų viena iš veislių.');
   {
-    // sprendimą|sprendimo pavyzdžius spausdinkite čia 
+    type DogBreed = Beagle | Poodle | Bulldog | GoldenRetriever | Dachshund;
+
+    const dog1: DogBreed = "beagle";
+    const dog2: DogBreed = "golden retriever";
+    const dog3: DogBreed = "bulldog";
+
+    console.log(`${dog1}, ${dog2}, ${dog3}`);
   }
   console.groupEnd();
 
   console.group('3. Sukurkite konkrečias 5 mašinos dalių tipus. Sukurkite tipą, kurio reikšmė būtų viena iš dalių.');
   {
-    // sprendimą|sprendimo pavyzdžius spausdinkite čia 
+    type CarParts = Bumper | Radiator | Dashboard | Spoiler | Tire;
+
+    const carPart1: CarParts = "bumper";
+    const carPart2: CarParts = "tire";
+    const carPart3: CarParts = "radiator";
+
+    console.log(`${carPart1}, ${carPart2}, ${carPart3}`);
   }
   console.groupEnd();
 
